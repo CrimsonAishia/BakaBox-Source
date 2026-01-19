19de0a5b276ac1b235376315823a0f4f982b0fb1
Aishia <aishia@zohomail.cn>
2026-01-19 08:55:18 +0800

    perf(disk_image_cache_service): 调整图片缓存组件超时时间

--- lib/core/services/disk_image_cache_service.dart
+++ lib/core/services/disk_image_cache_service.dart
@@ -111,7 +111,7 @@ class DiskImageCacheService {
         url,
         options: Options(
           responseType: ResponseType.bytes,
-          receiveTimeout: const Duration(seconds: 30),
+          receiveTimeout: const Duration(seconds: 10),
         ),
       );
       
