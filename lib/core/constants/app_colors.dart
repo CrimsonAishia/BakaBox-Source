317082a618c8b4e7ff749eb76367642d74294acc
Aishia <aishia@zohomail.cn>
2026-07-12 00:23:16 +0800

    feat(project): 补充颜色常量

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -32,20 +32,105 @@ class AppColors {
   static const Color slate800 = Color(0xFF1E293B);
   static const Color slate900 = Color(0xFF0F172A);
 
+  static const Color red50 = Color(0xFFFEF2F2);
+  static const Color red100 = Color(0xFFFEE2E2);
+  static const Color red200 = Color(0xFFFECACA);
+  static const Color red300 = Color(0xFFFCA5A5);
+  static const Color red400 = Color(0xFFF87171);
   static const Color red500 = Color(0xFFEF4444);
   static const Color red600 = Color(0xFFDC2626);
+  static const Color red700 = Color(0xFFB91C1C);
+  static const Color red800 = Color(0xFF991B1B);
+  static const Color red900 = Color(0xFF7F1D1D);
 
+  static const Color blue50 = Color(0xFFEFF6FF);
+  static const Color blue100 = Color(0xFFDBEAFE);
+  static const Color blue200 = Color(0xFFBFDBFE);
+  static const Color blue300 = Color(0xFF93C5FD);
+  static const Color blue400 = Color(0xFF60A5FA);
   static const Color blue500 = Color(0xFF3B82F6);
+  static const Color blue600 = Color(0xFF2563EB);
+  static const Color blue700 = Color(0xFF1D4ED8);
+  static const Color blue800 = Color(0xFF1E40AF);
+  static const Color blue900 = Color(0xFF1E3A8A);
+
+  static const Color sky50 = Color(0xFFF0F9FF);
+  static const Color sky100 = Color(0xFFE0F2FE);
+  static const Color sky200 = Color(0xFFBAE6FD);
+  static const Color sky300 = Color(0xFF7DD3FC);
   static const Color sky400 = Color(0xFF38BDF8);
+  static const Color sky500 = Color(0xFF0EA5E9);
+  static const Color sky600 = Color(0xFF0284C7);
+  static const Color sky700 = Color(0xFF0369A1);
+  static const Color sky800 = Color(0xFF075985);
+  static const Color sky900 = Color(0xFF0C4A6E);
 
+  static const Color indigo50 = Color(0xFFEEF2FF);
+  static const Color indigo100 = Color(0xFFE0E7FF);
+  static const Color indigo200 = Color(0xFFC7D2FE);
+  static const Color indigo300 = Color(0xFFA5B4FC);
+  static const Color indigo400 = Color(0xFF818CF8);
   static const Color indigo500 = Color(0xFF6366F1);
+  static const Color indigo600 = Color(0xFF4F46E5);
+  static const Color indigo700 = Color(0xFF4338CA);
+  static const Color indigo800 = Color(0xFF3730A3);
+  static const Color indigo900 = Color(0xFF312E81);
+
+  static const Color violet50 = Color(0xFFF5F3FF);
+  static const Color violet100 = Color(0xFFEDE9FE);
+  static const Color violet200 = Color(0xFFDDD6FE);
+  static const Color violet300 = Color(0xFFC4B5FD);
+  static const Color violet400 = Color(0xFFA78BFA);
   static const Color violet500 = Color(0xFF8B5CF6);
+  static const Color violet600 = Color(0xFF7C3AED);
+  static const Color violet700 = Color(0xFF6D28D9);
+  static const Color violet800 = Color(0xFF5B21B6);
+  static const Color violet900 = Color(0xFF4C1D95);
 
+  static const Color emerald50 = Color(0xFFECFDF5);
+  static const Color emerald100 = Color(0xFFD1FAE5);
+  static const Color emerald200 = Color(0xFFA7F3D0);
+  static const Color emerald300 = Color(0xFF6EE7B7);
+  static const Color emerald400 = Color(0xFF34D399);
   static const Color emerald500 = Color(0xFF10B981);
-  static const Color green500 = Color(0xFF22C55E);
+  static const Color emerald600 = Color(0xFF059669);
+  static const Color emerald700 = Color(0xFF047857);
+  static const Color emerald800 = Color(0xFF065F46);
+  static const Color emerald900 = Color(0xFF064E3B);
 
+  static const Color green50 = Color(0xFFF0FDF4);
+  static const Color green100 = Color(0xFFDCFCE7);
+  static const Color green200 = Color(0xFFBBF7D0);
+  static const Color green300 = Color(0xFF86EFAC);
+  static const Color green400 = Color(0xFF4ADE80);
+  static const Color green500 = Color(0xFF22C55E);
+  static const Color green600 = Color(0xFF16A34A);
+  static const Color green700 = Color(0xFF15803D);
+  static const Color green800 = Color(0xFF166534);
+  static const Color green900 = Color(0xFF14532D);
+
+  static const Color amber50 = Color(0xFFFFFBEB);
+  static const Color amber100 = Color(0xFFFEF3C7);
+  static const Color amber200 = Color(0xFFFDE68A);
+  static const Color amber300 = Color(0xFFFCD34D);
   static const Color amber400 = Color(0xFFFBBF24);
   static const Color amber500 = Color(0xFFF59E0B);
+  static const Color amber600 = Color(0xFFD97706);
+  static const Color amber700 = Color(0xFFB45309);
+  static const Color amber800 = Color(0xFF92400E);
+  static const Color amber900 = Color(0xFF78350F);
+
+  static const Color orange50 = Color(0xFFFFF7ED);
+  static const Color orange100 = Color(0xFFFFEDD5);
+  static const Color orange200 = Color(0xFFFED7AA);
+  static const Color orange300 = Color(0xFFFDBA74);
+  static const Color orange400 = Color(0xFFFB923C);
+  static const Color orange500 = Color(0xFFF97316);
+  static const Color orange600 = Color(0xFFEA580C);
+  static const Color orange700 = Color(0xFFC2410C);
+  static const Color orange800 = Color(0xFF9A3412);
+  static const Color orange900 = Color(0xFF7C2D12);
+  
   static const Color orange = Color(0xFFFF9800); // Material Orange 500
 
   /// 角色图鉴技能绿
